8a0fac017249f6ceb8b989cefa9839a0d984c88f
sesaal <sesaal@ttu.ee>
2020-10-17 11:21:15 +0300

    Tweaks and environmental builds

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/router'
+import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueCookie from 'vue-cookie'
